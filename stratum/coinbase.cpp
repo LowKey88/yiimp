ca8e16706e6bdc377c7204d68e09fb4203927e05
HisyamNasir <hisyamnasir@gmail.com>
2017-11-15 12:03:34 +0800

    Update coinbase.cpp

--- stratum/coinbase.cpp
+++ stratum/coinbase.cpp
@@ -208,6 +208,7 @@ void coinbase_create(YAAMP_COIND *coind, YAAMP_JOB_TEMPLATE *templ, json_value *
 		strcmp(coind->symbol, "VIVO") == 0 || // VIVO coin
 	   	strcmp(coind->symbol, "INN") == 0 || // Innova coin
 	   	strcmp(coind->symbol, "DSR") == 0 || // Desire coin
+	   	strcmp(coind->symbol, "GBX") == 0 || // GoByte coin
 		strcmp(coind->symbol, "DASH") == 0 || strcmp(coind->symbol, "DASH-TESTNET") == 0) // Dash 12.1
 	{
 		char script_dests[2048] = { 0 };
